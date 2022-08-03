dab2980313be8f88e895fae9ab6bc873cf1e5bc8
ocean <haeseok.yun@smartfoodnet.com>
2022-08-03 16:41:42 +0900

    feat: ApplicationEventPublisher 테스트

--- src/main/kotlin/me/ocean/kotlintemplate/AppRunner.kt
+++ src/main/kotlin/me/ocean/kotlintemplate/AppRunner.kt
@@ -2,10 +2,16 @@ package me.ocean.kotlintemplate
 
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.transaction.event.TransactionalEventListener
 
 @Component
-class AppRunner: ApplicationRunner {
+class AppRunner(
+    private val eventPublisher: EventPublisher,
+): ApplicationRunner {
     override fun run(args: ApplicationArguments?) {
         val test = Test1("haeseoky")
 
@@ -21,9 +27,58 @@ class AppRunner: ApplicationRunner {
 
         val main = Main("main", Sub("sub"))
         println(main)
+
+//        eventPublisher.eventPublish()
+        eventPublisher.transactionalEventPublish()
+    }
+
+
+
+}
+
+@Component
+class EventPublisher(
+    private val applicationEventPublisher: ApplicationEventPublisher,
+){
+    fun eventPublish() {
+        val event = Event("event", "event publish test")
+
+        println("eventPublish() start")
+        Thread.sleep(1000)
+        applicationEventPublisher.publishEvent(event)
+        Thread.sleep(1000)
+        println("eventPublish() published")
+    }
+
+    @Transactional
+    fun transactionalEventPublish() {
+        val event = Event("event", "event publish test")
+
+        println("transactionalEventPublish() start")
+        Thread.sleep(1000)
+        applicationEventPublisher.publishEvent(event)
+        Thread.sleep(1000)
+        println("transactionalEventPublish() published")
+    }
+}
+@Component
+class MyEventListener(){
+    @EventListener
+    fun eventListener(event: Event) {
+        println("event listen : $event")
+    }
+
+    @TransactionalEventListener
+    fun transactionalEventListener(event: Event) {
+        println("transactional event listen : $event")
     }
 }
 
+
+data class Event(
+    val name: String,
+    val description: String,
+)
 
 data class Main(
     val name: String,
