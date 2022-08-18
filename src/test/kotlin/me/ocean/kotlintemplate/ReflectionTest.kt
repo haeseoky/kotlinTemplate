5bb57962b2a69e75f0fadf3dcfa48bb2df6ca507
ocean <haeseok.yun@smartfoodnet.com>
2022-08-18 10:35:17 +0900

    feat: reflection test

---
+++ src/test/kotlin/me/ocean/kotlintemplate/ReflectionTest.kt
@@ -0,0 +1,21 @@
+package me.ocean.kotlintemplate
+
+import org.junit.jupiter.api.Test
+
+class ReflectionTest {
+    @Test
+    fun reflectionTest(){
+        val storageItemUpdateModel = Product(
+            name = "ocean", price = 10000.0, tf = null, age = 10, list = listOf("a", "b", "c")
+        )
+        val clazz = storageItemUpdateModel.javaClass
+
+        clazz.declaredFields.forEach {
+            it.isAccessible = true
+            println(it.name)
+            println(it[storageItemUpdateModel])
+        }
+
+
+    }
+}
